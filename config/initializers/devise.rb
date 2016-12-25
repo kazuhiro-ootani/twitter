52e302af1d0e8b2091a8d5562c360b890cc29976
kazuhiro-ootani <administrator@user.local>
2016-12-25 16:07:50 +0900

    d

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,3 +272,8 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
+
+Devise.setup do |config|
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
+end
+
