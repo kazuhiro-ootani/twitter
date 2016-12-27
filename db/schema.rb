9bfdbbf2dd4f3fe0e995e6723a814e4759678367
kazuhiro-ootani <administrator@user.local>
2016-12-27 14:26:11 +0900

    add column user_id for TweetsTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224035508) do
+ActiveRecord::Schema.define(version: 20161226092642) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "text",       limit: 140, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 20161224035508) do
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
+  add_foreign_key "tweets", "users"
 end
