cf38ce892961ca2deab762a912a12d91235c16aa
kazuhiro-ootani <administrator@user.local>
2017-01-17 20:00:51 +0900

    invalid  default_value_for

--- app/uploaders/user_img_uploader.rb
+++ app/uploaders/user_img_uploader.rb
@@ -17,12 +17,10 @@ class UserImgUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+  
+    "https://s3-ap-northeast-1.amazonaws.com/dev-chatspaces3/uploads/user/user_img/user_img.jpg"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
