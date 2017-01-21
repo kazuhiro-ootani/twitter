f06e61f2044bd83288ed367a7fef120b714858f7
kazuhiro-ootani <administrator@user.local>
2017-01-21 14:48:01 +0900

    delete description

--- app/controllers/followed_controller.rb
+++ app/controllers/followed_controller.rb
@@ -2,8 +2,7 @@ class FollowedController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    user = User.find(params[:id])
-    @followed = user.followers
+    @followed = @user.followers
     render layout: 'show.application'
   end
 end
