0ea0a506df50cc69114abd9781c760dfdbfb4d6f
kazuhiro-ootani <administrator@user.local>
2017-01-30 18:09:55 +0900

    create twee-box.js

---
+++ app/assets/javascripts/tweet-box.js
@@ -0,0 +1,15 @@
+$(function(){
+$('.fa-commenting-o').on('click', function(){
+  var textField = $(".message-input");
+  textField.val('@管理者 ');
+});
+$('.fa-bar-chart').on('click', function(){
+  alert("チャート！");
+});
+$('.fa-picture-o').on('click', function(){
+  alert("写真！");
+});
+  $('.fa-camera.new_camera').on('click', function(){
+  alert("カメラ!");
+});
+});
